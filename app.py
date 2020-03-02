4e50c893d301bd5ccc795ba76c580d0b8d4fd8e8
dilsada <dilsadakkoyun@gmail.com>
2020-03-03 01:26:09 +0300

    add css

--- app.py
+++ app.py
@@ -47,16 +47,5 @@ def index():
         return render_template("index.html", districts = districts, parkInfo = '')
     
 
-'''
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['qrname']
-    qrcolour = request.form['qrList']
-    background = request.form['backgroundList']
-    url = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + str(text) + '&color=' + str(qrcolour) + '&bgcolor=' + str(background) 
-    return render_template("test.html", qr_image = url)
-'''
-
 if __name__ == "__main__":
-    # get the status of the response
     app.run()
